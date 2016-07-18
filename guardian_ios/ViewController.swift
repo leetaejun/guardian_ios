ee05dd1c76893691045210ac508c3b04ee26d9b1
leetaejun <leetaejun147@gmail.com>
2016-07-18 21:52:25 +0900

    Initial Commit

---
+++ guardian_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  guardian_ios
+//
+//  Created by lee taejun on 2016. 7. 18..
+//  Copyright © 2016년 lee taejun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
